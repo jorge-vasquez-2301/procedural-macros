343566f397695b14264a448444c50a6c7b5db932
Jorge Vasquez <jv2301@gmail.com>
2023-08-25 10:38:12 -0400

    Use ToTokens trait in make-public macro

--- make-public-macro/src/lib.rs
+++ make-public-macro/src/lib.rs
@@ -1,6 +1,30 @@
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields, FieldsNamed};
+use quote::{quote, ToTokens};
+use syn::{
+    parse_macro_input, Data, DataStruct, DeriveInput, Field, Fields, FieldsNamed, Ident, Type,
+};
+
+struct StructField {
+    name: Ident,
+    ty: Type,
+}
+
+impl StructField {
+    fn new(field: &Field) -> Self {
+        Self {
+            name: field.ident.as_ref().unwrap().clone(),
+            ty: field.ty.clone(),
+        }
+    }
+}
+
+impl ToTokens for StructField {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        let n = &self.name;
+        let t = &self.ty;
+        quote!(pub #n: #t).to_tokens(tokens)
+    }
+}
 
 #[proc_macro_attribute]
 pub fn public(_attr: TokenStream, item: TokenStream) -> TokenStream {
@@ -15,11 +39,7 @@ pub fn public(_attr: TokenStream, item: TokenStream) -> TokenStream {
         _ => unimplemented!("Only works for structs with named fields"),
     };
 
-    let builder_fields = fields.iter().map(|f| {
-        let name = &f.ident;
-        let ty = &f.ty;
-        quote! { pub #name: #ty }
-    });
+    let builder_fields = fields.iter().map(StructField::new);
 
     let public_version = quote! {
         pub struct #name {
