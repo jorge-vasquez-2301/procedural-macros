03840709873f2a2859ce483d39a4b833ee3f6c99
Jorge Vasquez <jv2301@gmail.com>
2023-08-25 10:23:08 -0400

    Create make-public macro

---
+++ make-public-macro/src/lib.rs
@@ -0,0 +1,31 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields, FieldsNamed};
+
+#[proc_macro_attribute]
+pub fn public(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(item as DeriveInput);
+    let name = ast.ident;
+
+    let fields = match ast.data {
+        Data::Struct(DataStruct {
+            fields: Fields::Named(FieldsNamed { ref named, .. }),
+            ..
+        }) => named,
+        _ => unimplemented!("Only works for structs with named fields"),
+    };
+
+    let builder_fields = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        quote! { pub #name: #ty }
+    });
+
+    let public_version = quote! {
+        pub struct #name {
+            #(#builder_fields,)*
+        }
+    };
+
+    public_version.into()
+}
