dc38254357fc27bac99eaf76d440801bdd14523b
Jorge Vasquez <jv2301@gmail.com>
2023-08-25 15:23:10 -0400

    Add support for tuple structs to make-public macro

--- make-public-macro/src/lib.rs
+++ make-public-macro/src/lib.rs
@@ -3,13 +3,14 @@ use quote::{quote, ToTokens};
 use syn::{
     parse::{Parse, ParseStream},
     parse_macro_input,
-    punctuated::Punctuated,
     token::Colon,
-    Data, DataStruct, DeriveInput, Fields, FieldsNamed, Ident, Visibility,
+    Data, DataStruct, DeriveInput, Fields, FieldsNamed, FieldsUnnamed, Ident, Result, Type,
+    Visibility,
 };
 
+#[derive(Debug)]
 struct StructField {
-    name: Ident,
+    name: Option<Ident>,
     ty: Ident,
 }
 
@@ -17,18 +18,32 @@ impl ToTokens for StructField {
     fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
         let n = &self.name;
         let t = &self.ty;
-        quote!(pub #n: #t).to_tokens(tokens)
+        match n {
+            Some(n) => quote!(pub #n: #t),
+            None => quote!(pub #t),
+        }
+        .to_tokens(tokens)
     }
 }
 
 impl Parse for StructField {
-    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
-        let _vis: Result<Visibility, _> = input.parse();
-        let list = Punctuated::<Ident, Colon>::parse_terminated(input).unwrap();
-
-        Ok(StructField {
-            name: list.first().unwrap().clone(),
-            ty: list.last().unwrap().clone(),
+    fn parse(input: ParseStream) -> Result<Self> {
+        let _vis: Visibility = input.parse()?;
+        let ident = input.parse()?;
+        let colon: bool = input.peek(syn::token::Colon);
+        Ok(match colon {
+            true => {
+                let _colon: Colon = input.parse()?;
+                let ty = input.parse()?;
+                StructField {
+                    name: Some(ident),
+                    ty,
+                }
+            }
+            false => StructField {
+                name: None,
+                ty: ident,
+            },
         })
     }
 }
@@ -36,25 +51,44 @@ impl Parse for StructField {
 #[proc_macro_attribute]
 pub fn public(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(item as DeriveInput);
-    eprintln!("{:#?}", &ast);
+    // eprintln!("{:#?}", &ast);
 
     let name = ast.ident;
+    let is_named_struct;
 
     let fields = match ast.data {
         Data::Struct(DataStruct {
             fields: Fields::Named(FieldsNamed { ref named, .. }),
             ..
-        }) => named,
-        _ => unimplemented!("Only works for structs with named fields"),
+        }) => {
+            is_named_struct = true;
+            named
+        }
+        Data::Struct(DataStruct {
+            fields: Fields::Unnamed(FieldsUnnamed { ref unnamed, .. }),
+            ..
+        }) => {
+            is_named_struct = false;
+            unnamed
+        }
+        _ => unimplemented!("Only works for structs"),
     };
 
     let builder_fields = fields
         .iter()
         .map(|f| syn::parse2::<StructField>(f.to_token_stream()).unwrap());
 
-    let public_version = quote! {
-        pub struct #name {
-            #(#builder_fields,)*
+    let public_version = if is_named_struct {
+        quote! {
+            pub struct #name {
+                #(#builder_fields,)*
+            }
+        }
+    } else {
+        quote! {
+            pub struct #name(
+                #(#builder_fields,)*
+            );
         }
     };
 
--- src/bin/make-public.rs
+++ src/bin/make-public.rs
@@ -6,4 +6,7 @@ struct Example {
     pub second: u32,
 }
 
+#[public]
+struct Example2(String, pub u32);
+
 fn main() {}
