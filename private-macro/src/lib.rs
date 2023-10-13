e8d4b50e81f337af1e1c6a149a3a9e2ad8e39d4d
Jorge Vasquez <jv2301@gmail.com>
2023-10-13 14:52:54 -0400

    Improve private-macro

--- private-macro/src/lib.rs
+++ private-macro/src/lib.rs
@@ -2,28 +2,58 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::Data::Struct;
 use syn::Fields::Named;
-use syn::__private::{Span, TokenStream2};
 use syn::{parse_macro_input, DeriveInput};
 use syn::{DataStruct, FieldsNamed, Ident};
 
+struct ParsedField<'a> {
+    name: &'a syn::Ident,
+    ty: &'a syn::Type,
+}
+
+impl<'a> TryFrom<&'a syn::Field> for ParsedField<'a> {
+    type Error = &'static str;
+
+    fn try_from(field: &'a syn::Field) -> Result<Self, Self::Error> {
+        Ok(Self {
+            name: field.ident.as_ref().ok_or("field must have a name")?,
+            ty: &field.ty,
+        })
+    }
+}
+
 #[proc_macro]
 pub fn private(item: TokenStream) -> TokenStream {
-    let item_as_stream: quote::__private::TokenStream = item.clone().into();
     let ast = parse_macro_input!(item as DeriveInput);
     let name = &ast.ident;
-    let methods = generated_methods(&ast);
+
+    let parsed_fields = parse_fields(&ast);
+
+    let names = parsed_fields
+        .iter()
+        .map(|field| field.name)
+        .collect::<Vec<_>>();
+    let fields = generated_fields(&parsed_fields);
+    let methods = generated_methods(&parsed_fields);
 
     quote!(
-        #item_as_stream
+        struct #name {
+            #(#fields),*
+        }
 
         impl #name {
+            fn new(#(#fields),*) -> Self {
+                Self {
+                    #(#names),*
+                }
+            }
+
             #(#methods)*
         }
     )
     .into()
 }
 
-fn generated_methods(ast: &DeriveInput) -> Vec<TokenStream2> {
+fn parse_fields(ast: &DeriveInput) -> Vec<ParsedField> {
     let named_fields = match ast.data {
         Struct(DataStruct {
             fields: Named(FieldsNamed { ref named, .. }),
@@ -32,16 +62,29 @@ fn generated_methods(ast: &DeriveInput) -> Vec<TokenStream2> {
         _ => unimplemented!("Only works for structs with named fields"),
     };
 
-    named_fields
+    named_fields.iter().map(|f| f.try_into().unwrap()).collect()
+}
+
+fn generated_fields(parsed_fields: &[ParsedField]) -> Vec<proc_macro2::TokenStream> {
+    parsed_fields
         .iter()
-        .map(|f| {
-            let field_name = f.ident.as_ref().take().unwrap();
-            let type_name = &f.ty;
-            let method_name = Ident::new(&format!("get_{field_name}"), Span::call_site());
+        .map(|&ParsedField { name, ty }| {
+            quote!(
+                #name: #ty
+            )
+        })
+        .collect()
+}
+
+fn generated_methods(parsed_fields: &[ParsedField]) -> Vec<proc_macro2::TokenStream> {
+    parsed_fields
+        .iter()
+        .map(|&ParsedField { name, ty }| {
+            let method_name = Ident::new(&format!("get_{name}"), proc_macro2::Span::call_site());
 
             quote!(
-                fn #method_name(&self) -> &#type_name {
-                    &self.#field_name
+                pub fn #method_name(&self) -> &#ty {
+                    &self.#name
                 }
             )
         })
