5d86cd4014d831726b8b98e76b6d1b124c1bfef9
Jorge Vasquez <jv2301@gmail.com>
2023-12-01 12:02:18 -0400

    panic-to-result improvement

--- panic-to-result-macro/src/lib.rs
+++ panic-to-result-macro/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::{quote, ToTokens};
-use syn::{ItemFn, ReturnType, Stmt};
+use syn::{token::Semi, Expr, ItemFn, ReturnType, Stmt, StmtMacro};
 
 fn signature_output_as_result(ast: &ItemFn) -> ReturnType {
     let output = match ast.sig.output {
@@ -37,5 +37,58 @@ pub fn panic_to_result(_a: TokenStream, item: TokenStream) -> TokenStream {
         .stmts
         .push(last_statement_as_result(last_statement));
 
+    let new_statements: Vec<Stmt> = ast
+        .block
+        .stmts
+        .into_iter()
+        .map(|s| match s {
+            Stmt::Expr(e, t) => handle_expression(e, t),
+            _ => s,
+        })
+        .collect();
+    ast.block.stmts = new_statements;
+
     ast.to_token_stream().into()
+}
+
+fn handle_expression(expression: Expr, token: Option<Semi>) -> Stmt {
+    match expression {
+        Expr::If(mut ex_if) => {
+            let new_statements: Vec<Stmt> = ex_if
+                .then_branch
+                .stmts
+                .into_iter()
+                .map(|s| match s {
+                    Stmt::Macro(ref expr_macro) => extract_panic_content(expr_macro)
+                        .map(|t| {
+                            quote! {
+                                return Err(#t.to_string());
+                            }
+                        })
+                        .map(syn::parse2)
+                        .map(Result::unwrap)
+                        .unwrap_or(s),
+                    _ => s,
+                })
+                .collect();
+            ex_if.then_branch.stmts = new_statements;
+            Stmt::Expr(Expr::If(ex_if), token)
+        }
+        _ => Stmt::Expr(expression, token),
+    }
+}
+
+fn extract_panic_content(expr_macro: &StmtMacro) -> Option<proc_macro2::TokenStream> {
+    let does_panic = expr_macro
+        .mac
+        .path
+        .segments
+        .iter()
+        .any(|v| v.ident.to_string().eq("panic"));
+
+    if does_panic {
+        Some(expr_macro.mac.tokens.clone())
+    } else {
+        None
+    }
 }
