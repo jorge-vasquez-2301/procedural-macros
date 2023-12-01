83bfc8e95bc3a3105b98514ca5d3d7b87485410f
Jorge Vasquez <jv2301@gmail.com>
2023-12-01 16:50:41 -0400

    Improve error handling for panic-to-result using proc-macro-error

--- panic-to-result-macro/src/lib.rs
+++ panic-to-result-macro/src/lib.rs
@@ -1,8 +1,9 @@
 use proc_macro::TokenStream;
+use proc_macro_error::{emit_error, proc_macro_error};
 use quote::{quote, ToTokens};
-use syn::{spanned::Spanned, token::Semi, Expr, ItemFn, ReturnType, Stmt, StmtMacro};
+use syn::{token::Semi, Expr, ItemFn, ReturnType, Stmt, StmtMacro};
 
-fn signature_output_as_result(ast: &ItemFn) -> Result<ReturnType, syn::Error> {
+fn signature_output_as_result(ast: &ItemFn) -> ReturnType {
     let output = match ast.sig.output {
         ReturnType::Default => {
             quote! {
@@ -11,23 +12,24 @@ fn signature_output_as_result(ast: &ItemFn) -> Result<ReturnType, syn::Error> {
         }
         ReturnType::Type(_, ref ty) => {
             if ty.to_token_stream().to_string().contains("Result") {
-                return Err(
-                    syn::Error::new(
-                        ast.sig.span(), 
-                        format!(
-                            "this macro can only be applied to a function that does not return a Result. Signature: {}",
-                            quote!(#ty)
-                        ) 
+                emit_error!(
+                    ty,
+                    format!(
+                        "this macro can only be applied to a function \
+                        that does not yet return a Result. Signature: {}",
+                        quote!(#ty)
                     )
                 );
-            }
-            quote! {
-                -> Result<#ty, String>
+                ast.sig.output.to_token_stream()
+            } else {
+                quote! {
+                    -> Result<#ty, String>
+                }
             }
         }
     };
 
-    Ok(syn::parse2(output).unwrap())
+    syn::parse2(output).unwrap()
 }
 
 fn last_statement_as_result(last_statement: Option<Stmt>) -> Stmt {
@@ -38,54 +40,36 @@ fn last_statement_as_result(last_statement: Option<Stmt>) -> Stmt {
     Stmt::Expr(syn::parse2(last_modified).unwrap(), None)
 }
 
+#[proc_macro_error]
 #[proc_macro_attribute]
 pub fn panic_to_result(_a: TokenStream, item: TokenStream) -> TokenStream {
     let mut ast: ItemFn = syn::parse(item).unwrap();
 
-    let signature_output =  signature_output_as_result(&ast);
+    ast.sig.output = signature_output_as_result(&ast);
 
     let last_statement = ast.block.stmts.pop();
     ast.block
         .stmts
         .push(last_statement_as_result(last_statement));
 
-    let new_statements: Result<Vec<Stmt>, syn::Error> = ast
+    let new_statements = ast
         .block
         .stmts
         .into_iter()
         .map(|s| match s {
             Stmt::Expr(e, t) => handle_expression(e, t),
-            _ => Ok(s),
+            _ => s,
         })
         .collect();
-
-    match (new_statements, signature_output)  { 
-        (Ok(new), Ok(output)) => {
-            ast.block.stmts = new;
-            ast.sig.output = output;
-        }, 
-        (Ok(_), Err(signature_err)) => {
-            return signature_err.to_compile_error()
-                .into() 
-        },
-        (Err(statement_err), Ok(_)) => {
-            return statement_err.to_compile_error()
-                .into() 
-        },
-        (Err(mut statement_err), Err(signature_err)) => {
-            statement_err.combine(signature_err);
-            return statement_err.to_compile_error()
-                .into() 
-        }
-    };
+    ast.block.stmts = new_statements;
 
     ast.to_token_stream().into()
 }
 
-fn handle_expression(expression: Expr, token: Option<Semi>) -> Result<Stmt, syn::Error> {
+fn handle_expression(expression: Expr, token: Option<Semi>) -> Stmt {
     match expression {
         Expr::If(mut ex_if) => {
-            let new_statements: Result<Vec<Stmt>, syn::Error> = ex_if
+            let new_statements: Vec<Stmt> = ex_if
                 .then_branch
                 .stmts
                 .into_iter()
@@ -94,12 +78,15 @@ fn handle_expression(expression: Expr, token: Option<Semi>) -> Result<Stmt, syn:
                         let output = extract_panic_content(expr_macro);
 
                         if output.map(|v| v.is_empty()).unwrap_or(false) {
-                            Err(syn::Error::new(
-                                expr_macro.span(),
-                                "please make sure every panic in your function has a message",
-                            ))
+                            emit_error!(
+                                expr_macro,
+                                "panic needs a message!".to_string();
+                                help = "try to add a message: panic!(\"Example\".to_string())";
+                                note = "we will add the message to Result's Err"
+                            );
+                            s
                         } else {
-                            Ok(extract_panic_content(expr_macro)
+                            extract_panic_content(expr_macro)
                                 .map(|t| {
                                     quote! {
                                         return Err(#t.to_string());
@@ -107,18 +94,18 @@ fn handle_expression(expression: Expr, token: Option<Semi>) -> Result<Stmt, syn:
                                 })
                                 .map(syn::parse2)
                                 .map(Result::unwrap)
-                                .unwrap_or(s))
+                                .unwrap_or(s)
                         }
                     }
-                    _ => Ok(s),
+                    _ => s,
                 })
                 .collect();
 
-            ex_if.then_branch.stmts = new_statements?;
+            ex_if.then_branch.stmts = new_statements;
 
-            Ok(Stmt::Expr(Expr::If(ex_if), token))
+            Stmt::Expr(Expr::If(ex_if), token)
         }
-        _ => Ok(Stmt::Expr(expression, token)),
+        _ => Stmt::Expr(expression, token),
     }
 }
 
--- src/bin/panic-to-result.rs
+++ src/bin/panic-to-result.rs
@@ -7,7 +7,7 @@ struct Person {
 }
 
 #[panic_to_result]
-fn create_person_with_empty_panic(name: String, age: u32) -> Person {
+fn create_person_with_empty_panic(name: String, age: u32) -> Result<Person, ()> {
     if age > 30 {
         panic!();
     }
