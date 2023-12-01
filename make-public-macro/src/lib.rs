142145e5322c38b10b8dd9b7dcf5ce391ac5ffc3
Jorge Vasquez <jv2301@gmail.com>
2023-12-01 17:12:48 -0400

    Simplify make-public using mutability

--- make-public-macro/src/lib.rs
+++ make-public-macro/src/lib.rs
@@ -1,129 +1,35 @@
 use proc_macro::TokenStream;
-use quote::{quote, ToTokens};
+use quote::ToTokens;
 use syn::{
-    parse::{Parse, ParseStream},
-    parse_macro_input,
-    punctuated::Punctuated,
-    token::{Colon, Comma},
-    Data, DataEnum, DataStruct, DeriveInput, Field, Fields, FieldsNamed, FieldsUnnamed, Ident,
-    Result, Variant, Visibility,
+    parse_macro_input, spanned::Spanned, token::Pub, Data, DataStruct, DeriveInput, Fields,
+    FieldsNamed, FieldsUnnamed, Visibility,
 };
-
-enum ItemType {
-    NamedStruct,
-    TupleStruct,
-    Enum,
-}
-
-#[derive(Debug)]
-enum Elements {
-    Fields(Punctuated<Field, Comma>),
-    Variants(Punctuated<Variant, Comma>),
-}
-
-#[derive(Debug)]
-struct StructField {
-    name: Option<Ident>,
-    ty: Ident,
-}
-
-impl ToTokens for StructField {
-    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
-        let n = &self.name;
-        let t = &self.ty;
-        match n {
-            Some(n) => quote!(pub #n: #t),
-            None => quote!(pub #t),
-        }
-        .to_tokens(tokens)
-    }
-}
-
-impl Parse for StructField {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let _vis: Visibility = input.parse()?;
-        let ident = input.parse()?;
-        let colon: bool = input.peek(syn::token::Colon);
-        Ok(match colon {
-            true => {
-                let _colon: Colon = input.parse()?;
-                let ty = input.parse()?;
-                StructField {
-                    name: Some(ident),
-                    ty,
-                }
-            }
-            false => StructField {
-                name: None,
-                ty: ident,
-            },
-        })
-    }
-}
 
 #[proc_macro_attribute]
 pub fn public(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let ast = parse_macro_input!(item as DeriveInput);
+    let mut ast = parse_macro_input!(item as DeriveInput);
+    ast.vis = Visibility::Public(Pub(ast.span()));
     // eprintln!("{:#?}", &ast);
 
-    let name = ast.ident;
-    let attributes = ast.attrs;
-    let item_type;
-
-    let elements = match ast.data {
+    match ast.data {
         Data::Struct(DataStruct {
-            fields: Fields::Named(FieldsNamed { ref named, .. }),
+            fields: Fields::Named(FieldsNamed { ref mut named, .. }),
             ..
-        }) => {
-            item_type = ItemType::NamedStruct;
-            Elements::Fields(named.clone())
-        }
+        }) => named
+            .iter_mut()
+            .for_each(|field| field.vis = Visibility::Public(Pub(field.span()))),
         Data::Struct(DataStruct {
-            fields: Fields::Unnamed(FieldsUnnamed { ref unnamed, .. }),
+            fields:
+                Fields::Unnamed(FieldsUnnamed {
+                    ref mut unnamed, ..
+                }),
             ..
-        }) => {
-            item_type = ItemType::TupleStruct;
-            Elements::Fields(unnamed.clone())
-        }
-        Data::Enum(DataEnum { variants, .. }) => {
-            item_type = ItemType::Enum;
-            Elements::Variants(variants)
-        }
+        }) => unnamed
+            .iter_mut()
+            .for_each(|field| field.vis = Visibility::Public(Pub(field.span()))),
+        Data::Enum(..) => (),
         _ => unimplemented!("Only works for structs/enums"),
     };
 
-    let public_version = match elements {
-        Elements::Fields(fields) => {
-            let builder_fields = fields
-                .iter()
-                .map(|f| syn::parse2::<StructField>(f.to_token_stream()).unwrap());
-
-            match item_type {
-                ItemType::NamedStruct => quote! {
-                    #(#attributes)*
-                    pub struct #name {
-                        #(#builder_fields,)*
-                    }
-                },
-                ItemType::TupleStruct => quote! {
-                    #(#attributes)*
-                    pub struct #name(
-                        #(#builder_fields,)*
-                    );
-                },
-                ItemType::Enum => unreachable!(),
-            }
-        }
-        Elements::Variants(variants) => {
-            let variants = variants.iter();
-            quote! {
-                #(#attributes)*
-                pub enum #name {
-                    #(#variants,)*
-                }
-            }
-        }
-    };
-
-    public_version.into()
+    ast.to_token_stream().into()
 }
